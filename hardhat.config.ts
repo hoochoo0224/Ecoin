38e990723e07e0b9339e30ed54918c2a7180ba15
Jooowon Ko <83201165+hoochoo0224@users.noreply.github.com>
2024-12-22 22:30:30 +0900

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,8 +5,8 @@ const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     amoy: {
-      url: `https://polygon-amoy.g.alchemy.com/v2/0q3wLj-txnVrtrV8BTix_yYTgB3l3va3`,
-      accounts: ["b9c42440da896b010607d0c9b5d0059851f8fa6548513a5dd2008d6de79c4f38"],
+      url: `https://polygon-amoy.g.alchemy.com/v2/`,
+      accounts: [""],
     }
   }
 };
